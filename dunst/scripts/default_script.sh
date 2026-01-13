32dfe3141d05120a8dfa96ef9818e76caea6e860
lukavr05 <lvrooyen@yahoo.com>
2026-01-13 15:04:22 +0000

    finally integrated dunst

---
+++ dunst/scripts/default_script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Example notification script
+# You can add custom notification handling here
+
+case $1 in
+    button_left)
+        # Left click action
+        ;;
+    button_middle)
+        # Middle click action
+        ;;
+    button_right)
+        # Right click action
+        ;;
+esac
