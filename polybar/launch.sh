b0e1cf9626d5d89f8503d755d9f634a11cd0811d
lukavr05 <lvrooyen@yahoo.com>
2026-03-10 12:15:01 +0000

    updated polybar with separate bars

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-# Kill existing polybar instances
 killall -q polybar
 
-# Wait for processes to terminate
 while pgrep -x polybar >/dev/null; do sleep 0.1; done
 
-
-
-# Launch polybar
-polybar top-bar &
+polybar left &
+polybar center &
+polybar right &
